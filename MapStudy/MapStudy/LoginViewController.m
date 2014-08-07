5cccf065feeff7e378a5423126fda3ce72500396
dw <yunzhongyang@yunzhongyangdeMac-mini.local>
2014-08-07 09:56:45 +0800

    update LoginViewController

--- MapStudy/MapStudy/LoginViewController.m
+++ MapStudy/MapStudy/LoginViewController.m
@@ -27,7 +27,7 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    [self.view setBackgroundColor:[UIColor purpleColor]];
+
 }
 
 - (void)didReceiveMemoryWarning
